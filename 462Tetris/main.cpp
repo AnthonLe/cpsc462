6547fc46d6a3dc534b021063f51692da87be1d34
Sarah Nuno <saritanu321@yahoo.com>
2018-10-30 10:32:14 -0700

    Added do-while loop for login menu

--- 462Tetris/main.cpp
+++ 462Tetris/main.cpp
@@ -7,23 +7,39 @@
 //#include <conio.h> //for kbhit and getch functions
 
 #include "menu.h"
+#include "loginMenu.h"
 using namespace std;
 
-int main() {
+int main() 
+{
 
 	//create Brain object to change settings
 	Brain *brainTest = new Brain();
-
-
+	
+	loginMenu Tester;
 	Menu Test;
+	int ch = 0;
 	int c = 0;
-	do {
-		Test.DisplayWelcomeInterface();
-		cin >> c;
-		//create Game object to begin a new game
-		Game *gameTest = new Game();
-		Test.SetChoice(*gameTest, *brainTest, c);
-	} while (c != 3);
+	
+	//be shown the login menu
+	do
+	{
+		Tester.DisplayLoginMenu();
+		cin >> ch;
+		Tester.SetChoice(ch);
+	} while (ch == 2);
+	
+	//complete if able to login correctly
+	if (ch == 1) {
+		do
+		{
+			Test.DisplayWelcomeInterface();
+			cin >> c;
+			//create Game object to begin a new game
+			Game *gameTest = new Game();
+			Test.SetChoice(*gameTest, *brainTest, c);
+		} while (c != 3);
+	}
 
 	cout << "BYE\n";
 
