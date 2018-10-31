bc7b8934243fe7477d20b51571afa447b5d3f141
Sarah Nuno <saritanu321@yahoo.com>
2018-10-31 12:23:12 -0700

    Updated the removePlayer() function

--- 462Tetris/admin.h
+++ 462Tetris/admin.h
@@ -10,10 +10,11 @@ class Admin
 {
 public:
 	int choice;
+	vector<string> plList;
 	void displayAdminMenu();
 	void SetChoice(Brain &, int);
-	void viewPlayerList(ifstream &);
-	void removePlayer();
+	void viewPlayerList(ifstream &, vector<string>&);
+	void removePlayer(ifstream &, vector<string>&, ofstream &);
 	
 private:
 	int DoChoice(Brain &, int);
