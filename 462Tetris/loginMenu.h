f85ff946fe80a5565b0bd7eca143062b9f2c9ac3
Sarah Nuno <saritanu321@yahoo.com>
2018-10-31 07:20:53 -0700

    updated login function
    added some set/get functions

--- 462Tetris/loginMenu.h
+++ 462Tetris/loginMenu.h
@@ -5,22 +5,28 @@ using namespace std;
 class Game;
 class Brain;
 
-
 class loginMenu
 {
 public:
 	int choice;
-	char userName[10];
-	char pword[10];
+	int adminPass = 0;
+	int userPass = 0;
+	char userName;
+	char pword;
 
 	void sendNAME();
 	void sendPWORD();
 	void DisplayLoginMenu();
 	void SetChoice(int);
-	void login(ifstream &);
+	void login(ifstream &, int &, int &);
 	void registerPlayer(ofstream & );
+	int getUserPass();
+	int getAdminPass();
+	void setUserPass(int &);
+	void setAdminPass(int &);
 
 private:
 	int DoChoice(int);
 };
+
 
